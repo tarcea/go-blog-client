39860515cecd98692b82a1768b9e83e858c68d5a
tarcea <tarcea@gmail.com>
2022-11-08 12:37:52 +0100

    basic styling added

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,33 +1,54 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import style from '../styles/Post.module.css';
 import PageNotFound from './PageNotFound';
 
 const Post = () => {
 	const params = useParams();
+	const navigate = useNavigate()
 	const [server] = useState(process.env.REACT_APP_SERVER_URL)
  
   const [post, setPost] = useState<Post>()
-console.log(post)
+	console.log(post)
   const getPosts = async () => {
-    const a = await fetch(`${server}/posts/${params.id}`)
+    const a = await fetch(`${server}/posts/${params.id}`, {
+			method: 'GET',
+		})
     const res = await a.json()
     setPost(res)
   }
+	
+const deletePost = async () => {
+	await fetch(`${server}/posts/${params.id}`, {
+		method: 'DELETE',
+	})
+	navigate("/posts")
+}
+
   useEffect(() => {
     getPosts()
   },[])
   
   return (
 		<>
-	
-			<div >
-					{post && ( post.message ? <PageNotFound /> : ( 
-					<div key={post.id}>
-							<h3>{post.title}</h3>
-							<h4>by {post.author}</h4>
+			<div className={style.container}>
+				{post && ( 
+					post.message ?
+					<PageNotFound /> :
+					( 
+					<>
+					<div className={style.commands}>
+						<Link to="/posts" className={style.link}>Back</Link>
+						<Link to="/posts" onClick={deletePost} className={style.link}>Delete</Link>
+					</div>
+						<div key={post.id}>
+							<h3 className={style.by}>{post.author}</h3>
+							<h3 className={style.title}>{post.title}</h3>
 							<p>{post.body}</p>
 						</div>
-						))}
+					</>
+				)
+				)}
 			</div>
 		</>
   );
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import style from '../styles/PostList.module.css';
 
 const PostList = () => {
   const [server] = useState(process.env.REACT_APP_SERVER_URL)
@@ -17,22 +18,20 @@ console.log(posts)
   },[])
   
   return (
-    <div>
-      <header className="App-header">
-        <h1>Blog</h1>
-        <h2>Posts</h2>
+    <div className={style.container}>
+        <h1 className={style.header}>Bring your posts and build something great</h1>
         {posts && posts.map((post: Post) => (
-          <div key={post.id}>
+          <div key={post.id} className={style.content}>
             <Link to={`/posts/${post.id}`}>
               <h3>
                 {post.title}
               </h3>
               <h4>by {post.author}</h4>
-              <p>{post.body.slice(0, 100)}</p>
+              <p className={style.postBody}>{post.body.slice(0, 100)}</p>
             </Link>
           </div>
         ))}
-      </header>
+       
     </div>
   );
 }
